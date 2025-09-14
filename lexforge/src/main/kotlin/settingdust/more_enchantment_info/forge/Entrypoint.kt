57ba8a5506b00f9424077f00af59f9bc1f76fa99
SettingDust <settingdust@gmail.com>
2025-09-14 20:40:20 +0800

    ci: update publish

--- lexforge/src/main/kotlin/settingdust/more_enchantment_info/forge/Entrypoint.kt
+++
@@ -1,7 +0,0 @@
-package settingdust.more_enchantment_info.forge
-
-import net.minecraftforge.fml.common.Mod
-import settingdust.more_enchantment_info.MoreEnchantmentInfo
-
-@Mod(MoreEnchantmentInfo.MOD_ID)
-object MoreEnchantmentInfoLexforge {}
